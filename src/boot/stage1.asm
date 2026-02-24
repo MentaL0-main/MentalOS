3f3d0b137c5853a9ed9a585b079084a63e2108e3
Miraly Ruziev <obilev0000@gmail.com>
2026-02-24 15:17:37 +0300

    feat: Test colors

--- src/boot/stage1.asm
+++ src/boot/stage1.asm
@@ -1,27 +1,36 @@
 [org 0x7C00]
-
 mov ax, 0
 mov ds, ax
+
+mov ax, 0x0013
+int 0x10
+
+mov ax, 0xA000
 mov es, ax
+mov di, 0
 
-mov si, msg
-call print_string
-jmp $
+mov dx, 0
+.row:
+  mov bx, 0
+  .col:
+    mov al, dl
+    add al, bl
+    shr al, 2
+    stosb
+    inc bx
+    cmp bx, 320
+    jb .col
+  inc dx
+  cmp dx, 200
+  jb .row
 
-print_string:
-  mov ah, 0x0E
+kb_loop:
+  mov ah, 0x00
+  int 0x16
+  cmp al, 'q'
+  je $
+  jmp kb_loop
 
-.loop:
-  lodsb
-  test al, al
-  jz .done
-  int 0x10
-  jmp .loop
-
-.done:
-  ret
-
-msg db "MentalOS ONE", 0xA, 0x0
-
-times 510-($-$$) db 0x0
+times 510-($-$$) db 0
 dw 0xAA55
+
